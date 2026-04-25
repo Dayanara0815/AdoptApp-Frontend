a65ae4a331ef73d9f98e3fd30a19fd4ade1ee288
Dayanara <dayanaracarorojas@gmail.com>
2026-04-25 17:54:02 -0500

    Configuracion de Landing Page y CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import { useState } from 'react'
-import { Button } from 'react-bootstrap'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="container py-5">
-      <h1 className="mb-4">AdoptApp</h1>
-      <Button variant="primary" onClick={() => setCount(count + 1)}>
-        Count is {count}
-      </Button>
-    </div>
-  )
-}
-
-export default App
+import LandingPage from "./pages/LandingPage";
+  
+  export default function App() {
+    return <LandingPage />;
+  }
